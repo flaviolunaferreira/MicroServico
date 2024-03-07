bc67f30f12f79e181e5654b12c09790618a90d55
Flavio Luna Ferreira <flaviolunaferreira@gmail.com>
2024-03-07 17:13:19 -0300

    salvando alterações

--- organize/src/app/interface/Colunas.ts
+++ organize/src/app/interface/Colunas.ts
@@ -1,4 +1,12 @@
+import { ActivationStart } from "@angular/router";
+
 export interface Colunas {
   field: string;
   header: string;
+  position: string;
+  icon: string;
+  label: String;
+  type: String;
+  width: String;
+  height: String;
 }
